11d49b6e7fcb63c8916989ed19ac7ba9a318264a
deikas123 <deikasonline@gmail.com>
2025-02-28 12:47:29 +0300

    Resolved some build errors

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-cont userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
+
     _id:{ type: String, required: true},
     name: { type: String, required: true},
     email: { type: String, required: true, unique: true},
